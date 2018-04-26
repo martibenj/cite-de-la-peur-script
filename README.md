7dbbbe9b28589cda5b54096ff19431798ee7b562
bmartinelli <benjamin.martinelli-externe@bio-logic.net>
2018-04-26 13:50:28 +0200

    adding generation script for html output

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 ## Script de la Cité de la Peur
 ### Purpose
-The aim of this repo is only to store the [script](https://github.com/martibenj/cite-de-la-peur-script/blob/master/data/data.html) of "Le Cité de la Peur" movie.  
+The aim of this repo is only to store the [script](https://github.com/martibenj/cite-de-la-peur-script/blob/master/dist/html/data.html) of "Le Cité de la Peur" movie.
 => No copyright infringement intended
+
+### Generate outputs
+Differents outputs can be generated, use `npm install` before all :
+- `npm run gen` : generate all output files
+- `npm run html` : html file with encoding header
+- `npm run fortune` : fortune data file
 
 ### Links
 Using Github link feature, you can share a particular line like this :  
---
+++ scripts/gen-formats.js
@@ -0,0 +1,114 @@
+const fs = require('fs-extra');
+const path = require('path');
+const rimraf = require('rimraf');
+
+const {merge, reduce, set, take, isEmpty, map, find} = require('lodash/fp');
+
+const startToken = '%START%';
+const endToken = '%END%';
+const brTag = '<br>';
+
+const data = 'data';
+const filmRaw = 'data-film.raw';
+const creditsRaw = 'data-credits.raw';
+const filmHtml = 'data-film.html';
+const filmFortune = 'data-film.fortune';
+const dist = 'dist';
+const html = 'html';
+const fortune = 'fortune';
+const projRootPath = path.resolve('.');
+
+const rawFilmFilePath = path.resolve(projRootPath, data, filmRaw);
+const rawCreditsFilePath = path.resolve(projRootPath, data, creditsRaw);
+
+const distPath = path.resolve(projRootPath, dist);
+const distHtmlDirPath = path.resolve(projRootPath, dist, html);
+const distFortuneDirPath = path.resolve(projRootPath, dist, fortune);
+
+const htmlFilePath = path.resolve(distHtmlDirPath, filmHtml);
+const fortuneFilePath = path.resolve(distFortuneDirPath, filmFortune);
+
+const htmlStart = '<html><head><meta charset="UTF-8"></head><body><div id="script">';
+const htmlEnd = '</div></body></html>';
+
+/**
+ * Clean arborescence.
+ * @param pFormat
+ */
+const clean = (pFormat = null) => {
+  let pathToDelete = distPath;
+
+  if (pFormat && path.resolve(distPath, pFormat)) {
+    pathToDelete = path.resolve(distPath, pFormat);
+  }
+
+  console.log('Deleting ', pathToDelete);
+  rimraf(pathToDelete, () => null);
+};
+
+/**
+ * Generate arborescence.
+ * @param pFormat
+ */
+const genArbo = (pFormat = null) => {
+  let pathToCreate = distPath;
+
+  if (pFormat && path.resolve(distPath, pFormat)) {
+    pathToCreate = path.resolve(distPath, pFormat);
+  }
+
+  console.log('Creating ', pathToCreate);
+  fs.ensureDirSync(pathToCreate);
+};
+
+/** Generate HTML output file */
+const genHtml = () => {
+  clean(html);
+  genArbo(html);
+  fs.readFile(rawFilmFilePath, 'utf8', (err, pData) => {
+    Object.setPrototypeOf(pData, String.prototype);
+    if (pData && pData.length > 0) {
+      // Adding html tags
+      pData = htmlStart + pData + htmlEnd;
+
+      // Replacing tokens
+      pData = pData.replace(new RegExp(startToken, 'g'), '');
+      pData = pData.replace(new RegExp(endToken, 'g'), brTag);
+
+      // Writing file
+      fs.writeFileSync(htmlFilePath, pData, 'utf8');
+    }
+  });
+};
+
+/** Generate Fortune output file. */
+const genFortune = () => {
+  clean(fortune);
+  genArbo(fortune);
+};
+
+/** Generate all output files. */
+const genAll = () => {
+  clean();
+  genHtml();
+  genFortune();
+};
+
+const arg = find(e => e.includes('--format'), process.argv);
+if (!arg) {
+  // Default, Gen all
+  genAll();
+}
+else {
+  const format = arg.split('=')[1];
+  switch (format) {
+    case html:
+      genHtml();
+      break;
+    case fortune:
+      genFortune();
+      break;
+    default:
+      console.log('Unknown format, please use html or fortune or skip the use of the parameter format');
+  }
+}
